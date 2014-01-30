cb317e03f5696ca61e4335a47534108b68feb9ed
Steven Jacobs <stevejacobs@iname.com>
2014-01-30 14:26:53 -0500

    Update Constants.java
    Added comments and suggestions for naming conventions

--- src/edu/wpi/first/wpilibj/templates/Constants.java
+++ src/edu/wpi/first/wpilibj/templates/Constants.java
@@ -14,6 +14,9 @@ import edu.wpi.first.wpilibj.Talon;
  * @author Steven
  */
 public class Constants {
+    
+    // PWMs
+    // Let's change abbreviations to use words FRONT, REAR, LEFT, RIGHT
     public static final int PWM_DRIVE_FL = 4;
     public static final int PWM_DRIVE_RL = 3;
     public static final int PWM_DRIVE_FR = 2;
@@ -21,20 +24,34 @@ public class Constants {
     public static final int PWM_THROWER_RIGHT = 5;
     public static final int PWM_THROWER_LEFT = 6;
     
+    // DIOs
+    // Change thse names to DIO_THROWER_ENCODER_A & B
     public static final int ANGLE_CHANNEL_A = 1;
     public static final int ANGLE_CHANNEL_B = 2;
+    
+    // Analog Inputs
     public static final int SONIC_RIGHT_CHANNEL = 1;
     public static final int SONIC_LEFT_CHANNEL = 2;
     
+    // Relays
+    
+    
+    // Drive Motors
+    // Change these names to use Front/Rear and then Left/Right
     static Talon leftForward = new Talon(PWM_DRIVE_FL);
     static Talon leftBackward = new Talon(PWM_DRIVE_RL);
     static Talon rightForward = new Talon(PWM_DRIVE_FR);
     static Talon rightBackward = new Talon(PWM_DRIVE_RR);
     
+    // Joystick Buttons
     public static final int JB_DRIVE_SLOW = 2;
     
+    
+    // Miscellaneous
+    // Change name to TELEOP_LOOP_DELAY_SECS
     public static final double TIMER_DELAY_SECS = .01;
     
+    // Thrower parameters
     double[] throwerSpeedVals = new double[10];
     int[] throwerArcVals = new int[10];
     int[] throwerDistanceVals = new int[10];
