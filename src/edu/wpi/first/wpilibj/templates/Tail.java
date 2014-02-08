b2a225635c2d585587b9c678324fdea68b8f49ee
Steven Jacobs <stevejacobs@iname.com>
2014-02-07 23:18:26 -0500

    Update Tail.java
    Added documentation

--- src/edu/wpi/first/wpilibj/templates/Tail.java
+++ src/edu/wpi/first/wpilibj/templates/Tail.java
@@ -15,19 +15,21 @@ import edu.wpi.first.wpilibj.Jaguar;
  */
 public class Tail {
     
+    private double status;
     private double theta;
     private double stingerSpeed;
     private double baseSpeed;
-    private double voltsExtended = 4.45;
-    private double voltsRetracted = 3.35;
-    private double voltsEjectStinger = 3.57;
-    private double voltsStingerStart = 3.8;
+    // potentiometer values
+    private double voltsExtended = 4.45;       // fully extended
+    private double voltsRetracted = 3.35;      // fully retracted
+    private double voltsEjectStinger = 3.57;   // stinger eject start 
+    private double voltsStingerStart = 3.8;    // stinger pickup start
+    // tail base motor speeds at end points and direction
     private double beginRetractSpeed = -0.8;
     private double endRetractSpeed = 0.3;
     private double beginExtendSpeed = 0.8;
     private double endExtendSpeed = -0.22;
-    private double status = 0;
-    
+     
     Jaguar base = new Jaguar(Constants.PWM_TAIL_BASE);
     Jaguar stinger = new Jaguar(Constants.PWM_TAIL_STINGER);
     
@@ -37,44 +39,71 @@ public class Tail {
         
     }
     
+    //**
+         Set stinger motor speed
+    **//
     public void setStingerSpeed(double speed) {
         stingerSpeed = speed;
         stinger.set(stingerSpeed);
     }
     
+    //**
+         Current speed of stinger motor
+    **//
     public double getStingerSpeed() {
         return stingerSpeed;
     }
-    
+
+    //**
+         Set base motor speed
+    **//    
     public void setBaseSpeed(double speed) {
         baseSpeed = speed;
         base.set(baseSpeed);
     }
 
+    //**
+         Current speed set on base motor
+    **//
     public double getBaseSpeed() {
         return baseSpeed;
     }
 
-   
+    //**
+         Angle of tail measured in volts from potentiometer
+    **//
     public double getTheta() {
         theta = potentiometer.getVoltage();
         return theta;
     }
     
-    
+    //**
+         Initiate tail extension
+    **//
     public void startExtend() {
         status = Constants.TAIL_STATUS_EXTENDING;
     }
 
+    //**
+         Initiate tail retraction
+    **//
     public void startRetract() {
         status = Constants.TAIL_STATUS_RETRACTING;
     }
     
+    //**
+         Initiate ball ejection
+    **//
     public void startEject() {
         status = Constants.TAIL_STATUS_EJECTING;
     }
     
+    //**
+         Update tail and stinger movement
+         Call every robot loop cycle.
+    **//
     public void update() {
+        // tail movement
         if (status == Constants.TAIL_STATUS_RETRACTING) {
             updateRetract();
         } else if (status == Constants.TAIL_STATUS_EXTENDING
@@ -83,6 +112,7 @@ public class Tail {
         } else {
             setBaseSpeed(0);
         }
+        // stinger rotation
         if (getTheta() > voltsStingerStart 
                 && status != Constants.TAIL_STATUS_EJECTING) {
             setStingerSpeed(1.0);
@@ -94,7 +124,7 @@ public class Tail {
         }
     }
     
-    public void updateRetract() {
+    private void updateRetract() {
         double m = (endRetractSpeed - beginRetractSpeed) / (voltsRetracted - voltsExtended);
         setBaseSpeed(m * (getTheta() - voltsRetracted) + endRetractSpeed);
         if (getTheta() <= voltsRetracted) {
@@ -103,7 +133,7 @@ public class Tail {
         }
     }
     
-    public void updateExtend() {
+    private void updateExtend() {
         double m = (endExtendSpeed - beginExtendSpeed) / (voltsExtended - voltsRetracted);
         setBaseSpeed(m * (getTheta() - voltsExtended) + endExtendSpeed);
         if (getTheta() >= voltsExtended) {
